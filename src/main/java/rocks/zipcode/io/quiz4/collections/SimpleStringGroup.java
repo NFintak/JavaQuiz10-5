4956a097a970e3cb7145599782f0c61314e17297
Nathan <natefintak@gmail.com>
2025-05-02 14:08:19 -0400

    21/27 SimpleStringGroup tests passing

--- src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/SimpleStringGroup.java
@@ -1,31 +1,44 @@
 package rocks.zipcode.io.quiz4.collections;
 
+import java.util.ArrayList;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class SimpleStringGroup {
+
+    private ArrayList<String> iterables;
+
     public SimpleStringGroup() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        this.iterables = new ArrayList<String>();
+        //throw new UnsupportedOperationException("Method not yet implemented");
+    }
+
+    public SimpleStringGroup(ArrayList<String> iterables) {
+        this.iterables = iterables;
     }
 
     public Integer count() {
-        return null;
+        return iterables.size();
     }
 
     public void insert(String string) {
+        iterables.add(string);
     }
 
     public Boolean has(String string) {
-        return null;
+        return iterables.contains(string);
     }
 
     public String fetch(int indexOfValue) {
-        return null;
+        return iterables.get(indexOfValue);
     }
 
     public void delete(String string) {
+        iterables.remove(string);
     }
 
     public void clear() {
+        iterables.clear();
     }
 }

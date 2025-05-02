56cd935e0b70d8545d2d7b62fbecb1c88ebc0018
Nathan <natefintak@gmail.com>
2025-05-02 13:38:31 -0400

    student class tests passing

--- src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
@@ -4,17 +4,24 @@ package rocks.zipcode.io.quiz4.collections;
  * @author leon on 11/12/2018.
  */
 public class Student {
+
+    private Integer id;
+    private Double amountOfHours;
+
     public Student() {
         this(null);
     }
 
     public Student(Integer id) {
+        this.id = id;
+        this.amountOfHours = 0.0;
     }
 
     public void learn(Double amountOfHours) {
+        this.amountOfHours += amountOfHours;
     }
 
     public Double getTotalStudyTime() {
-        return null;
+        return amountOfHours;
     }
 }
